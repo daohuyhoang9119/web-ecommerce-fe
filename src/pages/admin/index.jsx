a5d69f343e6cd54c0205fec02a97bc96a5e8718a
HoangDH <daohuyhoang9119@gmail.com>
2022-12-27 21:05:40 +0700

    update

--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -4,13 +4,10 @@ import Auth from "./components/auth";
 import NoMatch from "./components/nomatch";
 import { Switch, Route, BrowserRouter, Redirect } from "react-router-dom";
 // import PrivateRoute from './components/auth/PrivateRoute';
-<<<<<<< HEAD
 import { getCookie } from "./function";
 
 import { NotificationContainer } from "react-notifications";
 import "react-notifications/lib/notifications.css";
-import "./admin-style.scss";
-import "./styles.scss";
 export default class Admin extends Component {
   render() {
     return (
@@ -19,31 +16,20 @@ export default class Admin extends Component {
           <NotificationContainer />
           <Switch>
             <Route path="/admin" component={rootRoutes} />
-            {/* {getCookie('token') ? <Route path='/admin' component={rootRoutes} /> : <Redirect to={"/auth/login"} />}
-=======
-
-import { getCookie } from './function';
-
-import { NotificationContainer } from "react-notifications";
-import "react-notifications/lib/notifications.css";
-
-
-export default class Admin extends Component {
-	render() {
-		return (
-			<div className="admin">
-				<BrowserRouter>
-				<NotificationContainer />
-					<Switch>
-						<Route path='/admin' component={rootRoutes} />
-						{/* {getCookie('token') ? <Route path='/admin' component={rootRoutes} /> : <Redirect to={"/auth/login"} />}
->>>>>>> nhatrang
-						{getCookie('token') ? <Redirect to={"/admin"} /> : <Redirect to={"/auth/login"} />}
-						<Route component={NoMatch} /> */}
+            {getCookie("token") ? (
+              <Route path="/admin" component={rootRoutes} />
+            ) : (
+              <Redirect to={"/auth/login"} />
+            )}
+            {getCookie("token") ? (
+              <Redirect to={"/admin"} />
+            ) : (
+              <Redirect to={"/auth/login"} />
+            )}
+            <Route component={NoMatch} />
           </Switch>
         </BrowserRouter>
       </div>
     );
   }
 }
-
